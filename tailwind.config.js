6514147dceab06c3a43f048d4bedc12313036093
Krissada Singhakachain <46844213+OmsinKrissada@users.noreply.github.com>
2022-07-03 08:27:52 +0700

    config tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: [
+    "pages/**/*.{js,jsx,ts,tsx}",
+    "components/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+      colors: {
+        white: colors.gray["100"],
+        gray: colors.gray,
+        dark: colors.gray["900"],
+      },
+      fontSize: {
+        "8xl": "6rem",
+      },
+      maxWidth: {
+        "2/5": "40%",
+        "2/3": "66.666667%",
+      },
+      maxHeight: {
+        "1/4": "25%",
+      },
+      minWidth: {
+        "1/4": "25%",
+        "1/3": "33.333333%",
+      },
+      minHeight: {
+        "1/4": "25%",
+        "1/2": "50%",
+      },
+      zIndex: {
+        "-10": "-10",
+      },
+    },
+  },
+  plugins: [],
+};
